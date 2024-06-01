191868f87768a238909fdac770ee0086a667207a
Mohammad Mamun <md.mamun.bd147@gmail.com>
2024-06-02 00:00:41 +0600

    first commit

---
+++ src/components/OrdersReview/OrdersReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersReview = () => {
+    return (
+        <div>
+            <h2>Orders Review page</h2>
+        </div>
+    );
+};
+
+export default OrdersReview;<h2>Orders Review page</h2>
